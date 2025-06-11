4c09a77c2f5848a02e1e1ddaaa57ee518044e18c
UCastellano <prof.ucastellanop@uml.edu.ni>
2025-06-11 13:39:17 -0600

    Nuevos cambios

--- app.js
+++ app.js
@@ -6,7 +6,11 @@ const fs = require('fs');
 const axios = require('axios');
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+}));
 app.use(express.json());
 
 // SIRVE ARCHIVOS ESTÁTICOS DEL FRONTEND
