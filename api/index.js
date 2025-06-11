b490c39aa85a3459dd4754cc2835256544d64d1f
UCastellano <prof.ucastellanop@uml.edu.ni>
2025-06-11 16:37:11 -0600

    Actualizacion

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const drive = require('./drive');
+const drive = require('../drive');
 const path = require('path');
 const fs = require('fs');
 const axios = require('axios');
--- drive.js
+++ drive.js
@@ -1,8 +1,10 @@
 const { google } = require('googleapis');
-const key = require('./drive-key.json');
 
 const auth = new google.auth.GoogleAuth({
-  credentials: key,
+  credentials: {
+    client_email: process.env.GOOGLE_CLIENT_EMAIL,
+    private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  },
   scopes: ['https://www.googleapis.com/auth/drive.readonly']
 });
 
